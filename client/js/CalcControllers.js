a9dd42ae6b3b00d9dae6c0bf64a4db2dc846d623
juandsierra82 <juandsierra82@gmail.com>
2015-12-11 16:32:30 -0600

    calculator creating numbers

--- client/js/CalcControllers.js
+++ client/js/CalcControllers.js
@@ -1,5 +1,19 @@
 angular.module('calculator.controller', ['calculator.services'])
 
-.controller('Rendering', function ($scope){
+.controller('Rendering', function ($scope, Ops, Display){
+	$scope.displayed = '0';
 	
+	$scope.result = {};
+	$scope.result.memory = "";
+	$scope.result.current = "";
+	$scope.result.total = "";
+
+	$scope.keyboard = {
+		numbers: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
+		operations: ['+', '-', '/', '*', '+/-', '=', 'CE', '<-'],
+		decimal: '.'
+	}
+	$scope.concat=function(value){
+		$scope.result.current+=value;
+	}
 })
--- client/js/Calculator.js
+++ client/js/Calculator.js
@@ -1,5 +1,65 @@
 angular.module('calculator.services', [])
 
-.service('Operations', function(){
-	
-})
+.factory('Ops', Ops)
+
+.factory('Display', Display)
+
+//basic operations
+function Ops (){
+		
+		function add(a, b){
+			return a+b;
+		}
+
+		function minus(a, b){
+			return a-b;
+		}
+
+		function multiply(a, b){
+			return a*b;
+		}
+
+		function divide(a, b){
+			if(b === '0') return 'NaN';
+			return a/b;
+		}
+
+		function negate(a){
+			return -a;
+		}
+
+		function clear(str){
+			return '0';
+		}
+
+	return {
+		add: add,
+		minus: minus,
+		multiply: multiply,
+		divide: divide,
+		negate: negate
+	}
+}
+
+function Display (){
+		
+	function round(str){
+		if(str.indexOf('.') === -1)
+			return str;
+
+		var rounded = ''; 
+		var strArr = str.split('.');
+		rounded =+ str[0] + strArr[1].slice(0, 5);
+		return rounded;
+		}
+
+	function backspace(str){
+		return str.substring(0, str.length-1);
+	}
+
+	return {
+
+		round: round,
+		backspace: backspace
+	}
+}
