098d002955a3172a6ca64d912a4b54e88cae0cb2
juandsierra82 <juandsierra82@gmail.com>
2015-12-11 16:50:04 -0600

    single decimal point

--- client/js/CalcControllers.js
+++ client/js/CalcControllers.js
@@ -9,11 +9,14 @@ angular.module('calculator.controller', ['calculator.services'])
 	$scope.result.total = "";
 
 	$scope.keyboard = {
-		numbers: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
+		numbers: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'],
 		operations: ['+', '-', '/', '*', '+/-', '=', 'CE', '<-'],
 		decimal: '.'
 	}
 	$scope.concat=function(value){
+		if(value === '.' && $scope.result.current.indexOf('.') > -1){
+			return ;
+		}
 		$scope.result.current+=value;
 	}
 })
