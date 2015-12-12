2df64b31aa5301571377f1c89747c136f087e2fe
juandsierra82 <juandsierra82@gmail.com>
2015-12-12 17:17:25 -0600

    basic calculator look set up

--- client/js/app.js
+++ client/js/app.js
@@ -2,7 +2,7 @@
 
 angular.module('calculator', ['calculator.controller', 'ngRoute', 'ui.router', 'ngMaterial'])
 
-.config(function ($stateProvider, $urlRouterProvider){
+.config(function ($stateProvider, $urlRouterProvider, $mdThemingProvider){
 	$urlRouterProvider.otherwise('/');
 
 	$stateProvider
@@ -12,5 +12,21 @@ angular.module('calculator', ['calculator.controller', 'ngRoute', 'ui.router', '
 			controller: 'Rendering'
 		})
 
+	$mdThemingProvider.theme('default')
+		.primaryPalette('light-blue', {
+			'default': '300',
+			'hue-1': '100', 
+      'hue-2': '600', 
+      'hue-3': 'A100' 
+		})
+
+		.accentPalette('indigo', {
+			'default': '300',
+			'hue-1': '100', 
+      'hue-2': '600', 
+      'hue-3': 'A100' 
+		})
+
+		.backgroundPalette('blue-grey')
 })
 
