f96c425a4a4403af3933f65c9b2d6d6937f57f0f
juandsierra82 <juandsierra82@gmail.com>
2015-12-12 09:11:59 -0600

    added backspace functionality; CE now clears pending and current operations

--- client/js/CalcControllers.js
+++ client/js/CalcControllers.js
@@ -6,22 +6,25 @@ angular.module('calculator.controller', ['calculator.services'])
 	$scope.result = {};
 	$scope.result.memory = null;
 	$scope.result.current = ""
-	$scope.result.total = "0"
 
 	$scope.operation = {}
 	$scope.operation.pending = null;
-	$scope.operation.current = null;
 
 //keyboard object
 	$scope.keyboard = {
-		numbers: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'],
+		numbers: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '<='],
 		operations: ['+', '-', '/', '*', '=', 'CE']
 	}
 //app functions
-//display
+//display controls; concat gets called whenever a user clicks on a button
 	$scope.concat=function(value){
-
+	//checks to see if '.' has been entered
 		if(value === '.' && $scope.result.current.indexOf('.') > -1){
+			return ;
+		}
+	//allows for editing of input
+		if(value === '<='){
+			$scope.result.current = Display.backspace($scope.result.current);
 			return ;
 		}
 		$scope.result.current+=value;	
@@ -32,13 +35,13 @@ angular.module('calculator.controller', ['calculator.services'])
 	$scope.do=function(value){		
 	//operation sets memory if non is set, and remembers operand
 
-	//once a new number is starting to type, total becomes memory
-
 		if($scope.result.memory === null){
 			$scope.result.memory = parseFloat($scope.result.current);
 			$scope.operation.pending = value;
 			$scope.result.current = Ops.clear();
 		} else {
+
+	//if there is a number in memory, the calculator then finishes the preceding operation
 
 			if($scope.operation.pending === '+'){
 				$scope.result.memory = Ops.add($scope.result.memory, parseFloat($scope.result.current));
@@ -56,21 +59,24 @@ angular.module('calculator.controller', ['calculator.services'])
 				$scope.result.memory = Ops.divide($scope.result.memory, parseFloat($scope.result.current));
 			}
 
+	//after preceding operation is done, memory is rounded off 
 			$scope.result.memory = Display.round($scope.result.memory);
 
-			if(value === 'CE'){
+	//checks to see if the operation needs to be returned, pending continues unchanged
+			if(value === '='){
+				return ;
+			}
+	//value reassigned to pending, and the current input is cleared waiting for new user input
+			$scope.operation.pending = value;
+			$scope.result.current = Ops.clear();
+	
+	//checks to see if the memory needs clear
+		if(value === 'CE'){
 				$scope.result.memory = null;
+				$scope.operation.pending = null;
 				$scope.result.total = Ops.clear();
 				return ;
 			}
-
-			if(value === '='){
-
-				return ;
-			}
-
-			$scope.operation.pending = value;
-			$scope.result.current = Ops.clear();
 		}	
 
 	}
